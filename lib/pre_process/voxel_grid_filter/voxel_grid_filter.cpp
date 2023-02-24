d942a46f90366f1d830cac488f31974dcd05504e
XiaoHu <42265454+Xiao-Hu-Z@users.noreply.github.com>
2023-02-24 09:29:47 +0800

    Update voxel_grid_filter.cpp

--- lib/pre_process/voxel_grid_filter/voxel_grid_filter.cpp
+++ lib/pre_process/voxel_grid_filter/voxel_grid_filter.cpp
@@ -1,8 +1,8 @@
 
 /*
- * @Author: zhao_xiaohu
+ * @Author: xiaohu
  * @Date: 2022-04-02 00:26:55
- * @Last Modified by: zhao_xiaohu
+ * @Last Modified by: xiaohu
  * @Last Modified time: 2022-04-02 01:12:59
  */
 #include "voxel_grid_filter.h"
