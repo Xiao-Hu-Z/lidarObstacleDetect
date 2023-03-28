b237c2f41a9104b7222c61f7ace2169c0f948637
XiaoHu <42265454+Xiao-Hu-Z@users.noreply.github.com>
2023-03-28 23:01:22 +0800

    Update lidar_detection_track.cpp

--- src/lidar_detection_track.cpp
+++ src/lidar_detection_track.cpp
@@ -1,9 +1,3 @@
-/*
- * @Author: zhao_xiaohu
- * @Date: 2022-04-02 00:26:55
- * @Last Modified by: zhao_xiaohu
- * @Last Modified time: 2022-04-02 01:12:59
- */
 
 #include "lidar_detection_track.h"
 
